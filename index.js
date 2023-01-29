0ee72a207132a9da76090c65031747f01128021e
David Polos <polosdav@gmail.com>
2023-01-29 05:45:40 +0100

    fixed menu bug

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function onClickMenu() {
   document.getElementById("menu").classList.toggle("change");
   document.getElementsByTagName("main")[0].classList.toggle("navOn");
+  document.body.classList.toggle("no-scroll");
 }
