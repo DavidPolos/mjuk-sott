b3a4730edf5c8301d583384051501af679a8c7e4
David Polos <polosdav@gmail.com>
2023-01-31 04:33:50 +0100

    testing fix for mobile header

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function onClickMenu() {
   document.getElementById("menu").classList.toggle("change");
   document.getElementsByTagName("main")[0].classList.toggle("navOn");
   document.body.classList.toggle("no-scroll");
+  document.getElementsByTagName("header")[0].classList.toggle("sticky-header");
 }
